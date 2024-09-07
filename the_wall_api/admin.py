88fc8d68cc0335e541a911c465e68244a819f1ef
Aleksandar Dimitrov <adimitrov3@dxc.com>
2024-09-07 11:18:46 +0300

    Additional search fields

--- the_wall_api/admin.py
+++ the_wall_api/admin.py
@@ -4,17 +4,17 @@ from the_wall_api.models import Wall, WallProfile, WallProfileProgress
 
 class WallAdmin(admin.ModelAdmin):
     list_filter = ('id', 'wall_config_hash')
-    search_fields = ('id', 'wall_config_hash', 'num_crews', 'total_cost', 'construction_days')
+    search_fields = ('id', 'wall_config_hash', 'num_crews', 'total_cost', 'construction_days', 'date_created')
 
 
 class WallProfileAdmin(admin.ModelAdmin):
     list_filter = ('id', 'wall', 'wall_profile_config_hash')
-    search_fields = ('id', 'wall_profile_config_hash')
+    search_fields = ('id', 'wall_profile_config_hash', 'profile_id', 'cost', 'date_created')
 
 
 class WallProfileProgressAdmin(admin.ModelAdmin):
     list_filter = ('wall_profile', 'day')
-    search_fields = ('id', 'day')
+    search_fields = ('id', 'day', 'ice_used', 'date_created')
 
 
 admin.site.register(Wall, WallAdmin)
