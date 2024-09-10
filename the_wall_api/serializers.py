2b2c6c28f56d18ec98ca7587a2ee65e4aa7c86c7
Aleksandar Dimitrov <adimitrov3@dxc.com>
2024-09-10 12:20:23 +0300

    Remove redundant serializers

--- the_wall_api/serializers.py
+++ the_wall_api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.core.validators import MinValueValidator
-from the_wall_api.models import WallProfile, WallProfileProgress
+from the_wall_api.models import WallProfile
 
 
 class DailyIceUsageSerializer(serializers.ModelSerializer):
@@ -20,22 +20,3 @@ class CostOverviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = WallProfile
         fields = ['profile_id', 'num_crews']
-
-
-class WallProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WallProfile
-        fields = '__all__'
-
-    def validate_wall_profile_config_hash(self, value):
-        if len(value) < 64:
-            raise serializers.ValidationError('wall_profile_config_hash must be exactly 64 characters long.')
-        return value
-
-
-class WallProfileProgressSerializer(serializers.ModelSerializer):
-    wall_profile = serializers.PrimaryKeyRelatedField(queryset=WallProfile.objects.all())
-
-    class Meta:
-        model = WallProfileProgress
-        fields = ['wall_profile', 'day', 'ice_used', 'cost']
