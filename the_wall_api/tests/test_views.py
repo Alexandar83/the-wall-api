288fad298e44d1b8000e98e00f0d1a950a1e2d5e
Aleksandar Dimitrov <adimitrov3@dxc.com>
2024-09-11 22:00:21 +0300

    Move the execute_test_case method in the base class

--- the_wall_api/tests/test_views.py
+++ the_wall_api/tests/test_views.py
@@ -4,11 +4,16 @@ from rest_framework import status
 from inspect import currentframe
 
 from the_wall_api.tests.test_utils import BaseTestcase, generate_valid_values, invalid_input_groups
-from the_wall_api.utils import load_wall_profiles_from_config, MULTI_THREADED
+from the_wall_api.utils import exposed_endpoints, load_wall_profiles_from_config, MULTI_THREADED
 from the_wall_api.wall_construction import WallConstruction
 
 
-class BaseWallProfileTest(BaseTestcase):
+class ViewTest(BaseTestcase):
+    
+    @property
+    def url_name(self):
+        """Subclasses should override this property to provide the correct url_name."""
+        raise NotImplementedError('Subclasses must define the `url_name` property.')
 
     def setUp(self):
         # Load the wall profiles configuration to determine the maximum valid profile_id
@@ -48,10 +53,45 @@ class BaseWallProfileTest(BaseTestcase):
         # Add 0 to test single threaded mode
         valid_num_crews.insert(0, 0)
         return valid_num_crews
+    
+    def execute_test_case(self, expected_status, test_case_source, profile_id=None, day=None, num_crews=None):
+        """Executes test cases for the different endpoints."""
+        # Reverse the URL
+        if profile_id is not None and day is not None:
+            # daily-ice-usage
+            url = reverse(self.url_name, kwargs={'profile_id': profile_id, 'day': day})
+        elif profile_id is not None:
+            # costoverview/profile_id
+            url = reverse(self.url_name, kwargs={'profile_id': profile_id})
+        else:
+            # costoverview
+            url = reverse(self.url_name)
+
+        # Prepare the query params
+        params = {'num_crews': num_crews} if num_crews is not None else {}
+
+        # Execute the GET request
+        response = self.client.get(url, params)
+        passed = response.status_code == expected_status
+        
+        input_data = {key: value for key, value in [('profile_id', profile_id), ('day', day), ('num_crews', num_crews)] if value is not None}
+
+        # Log the test result
+        self.log_test_result(
+            passed=passed,
+            input_data=input_data,
+            expected_message=expected_status,
+            actual_message=str(response.status_code),
+            test_case_source=test_case_source
+        )
 
 
-class DailyIceUsageViewTest(BaseWallProfileTest):
+class DailyIceUsageViewTest(ViewTest):
     description = 'Daily Ice Usage View Tests'
+    
+    @property
+    def url_name(self):
+        return exposed_endpoints['daily-ice-usage']['name']
 
     def test_daily_ice_usage_valid(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
@@ -62,95 +102,75 @@ class DailyIceUsageViewTest(BaseWallProfileTest):
             valid_days = self.get_valid_days_for_profile(profile_id)
             for day in valid_days:
                 for num_crews in valid_num_crews:
-                    self._execute_test_case(profile_id, day, num_crews, status.HTTP_200_OK, test_case_source)
+                    self.execute_test_case(status.HTTP_200_OK, test_case_source, profile_id, day, num_crews)
 
     def test_daily_ice_usage_invalid_profile_id(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
         invalid_profile_ids = self.get_invalid_profile_ids()
-        valid_days = generate_valid_values()
-        valid_num_crews = self.get_valid_num_crews()
+        day = generate_valid_values()[0]
+        num_crews = self.get_valid_num_crews()[0]
 
-        for profile_id in invalid_profile_ids:
-            self._execute_test_case(profile_id, valid_days[0], valid_num_crews[0], status.HTTP_400_BAD_REQUEST, test_case_source)
+        for invalid_profile_id in invalid_profile_ids:
+            self.execute_test_case(status.HTTP_400_BAD_REQUEST, test_case_source, invalid_profile_id, day, num_crews)
     
     def test_daily_ice_usage_invalid_day_single_threaded(self):
         """Test with days after the construction's completion day."""
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
-        valid_profile_id = self.get_valid_profile_ids()[0]
-        invalid_days = self.get_invalid_days_for_profile_single_threaded(valid_profile_id)
+        profile_id = self.get_valid_profile_ids()[0]
+        invalid_days = self.get_invalid_days_for_profile_single_threaded(profile_id)
         num_crews = 0
 
-        for day in invalid_days:
-            self._execute_test_case(valid_profile_id, day, num_crews, status.HTTP_400_BAD_REQUEST, test_case_source)
+        for invalid_day in invalid_days:
+            self.execute_test_case(status.HTTP_400_BAD_REQUEST, test_case_source, profile_id, invalid_day, num_crews)
     
     def test_daily_ice_usage_invalid_day_multi_threaded(self):
         """Test with days on which the profile was not worked on."""
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
-        valid_profile_id = 2
-        valid_num_crews = 1
-        invalid_days = self.get_invalid_days_for_profile_multi_threaded(valid_profile_id, valid_num_crews)
+        profile_id = 2
+        num_crews = 1
+        invalid_days = self.get_invalid_days_for_profile_multi_threaded(profile_id, num_crews)
 
-        for day in invalid_days:
-            self._execute_test_case(valid_profile_id, day, valid_num_crews, status.HTTP_404_NOT_FOUND, test_case_source)
+        for invalid_day in invalid_days:
+            self.execute_test_case(status.HTTP_404_NOT_FOUND, test_case_source, profile_id, invalid_day, num_crews)
     
     def test_daily_ice_usage_invalid_num_crews(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
-        valid_profile_id = self.get_valid_profile_ids()[0]
-        valid_day = self.get_valid_days_for_profile(valid_profile_id)[0]
+        profile_id = self.get_valid_profile_ids()[0]
+        day = self.get_valid_days_for_profile(profile_id)[0]
 
         for invalid_num_crews_group in invalid_input_groups['num_crews'].values():
-            for invalid_num_crews in invalid_num_crews_group:
-                if invalid_num_crews in ({}, []):  # Skip {} and [] in invalid checks - the serializer validates num_crews for them
-                    continue
-                self._execute_test_case(valid_profile_id, valid_day, invalid_num_crews, status.HTTP_400_BAD_REQUEST, test_case_source)
-
-    def _execute_test_case(self, profile_id, day, num_crews, expected_status, test_case_source):
-        url = reverse('daily-ice-usage-v1', kwargs={'profile_id': profile_id, 'day': day})
-        response = self.client.get(url, {'num_crews': num_crews})
-        passed = response.status_code == expected_status
-        self.log_test_result(
-            passed=passed,
-            input_data={'profile_id': profile_id, 'day': day, 'num_crews': num_crews},
-            expected_message=expected_status,
-            actual_message=str(response.status_code),
-            test_case_source=test_case_source
-        )
+            invalid_num_crews = invalid_num_crews_group[0]
+            self.execute_test_case(status.HTTP_400_BAD_REQUEST, test_case_source, profile_id, day, invalid_num_crews)
 
 
-class CostOverviewViewTest(BaseWallProfileTest):
+class CostOverviewViewTest(ViewTest):
     description = 'Cost Overview View Tests'
+    
+    @property
+    def url_name(self):
+        return exposed_endpoints['cost-overview']['name']
 
     def test_cost_overview_valid(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
         valid_num_crews = self.get_valid_num_crews()
 
         for num_crews in valid_num_crews:
-            self._execute_test_case(None, num_crews, status.HTTP_200_OK, test_case_source)
+            self.execute_test_case(status.HTTP_200_OK, test_case_source, num_crews=num_crews)
 
     def test_cost_overview_invalid_num_crews(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
 
         for invalid_num_crews_group in invalid_input_groups['num_crews'].values():
-            for invalid_num_crews in invalid_num_crews_group:
-                if invalid_num_crews in ({}, []):  # Skip {} and [] in invalid checks - the serializer validates num_crews for them
-                    continue
-                self._execute_test_case(None, invalid_num_crews, status.HTTP_400_BAD_REQUEST, test_case_source)
-
-    def _execute_test_case(self, profile_id, num_crews, expected_status, test_case_source):
-        url = reverse('cost-overview-v1')
-        response = self.client.get(url, {'num_crews': num_crews})
-        passed = response.status_code == expected_status
-        self.log_test_result(
-            passed=passed,
-            input_data={'profile_id': profile_id, 'num_crews': num_crews},
-            expected_message=expected_status,
-            actual_message=str(response.status_code),
-            test_case_source=test_case_source
-        )
+            invalid_num_crews = invalid_num_crews_group[0]
+            self.execute_test_case(status.HTTP_400_BAD_REQUEST, test_case_source, num_crews=invalid_num_crews)
 
 
-class CostOverviewProfileidViewTest(CostOverviewViewTest):
+class CostOverviewProfileidViewTest(ViewTest):
     description = 'Cost Overview Profileid View Tests'
+    
+    @property
+    def url_name(self):
+        return exposed_endpoints['cost-overview-profile']['name']
 
     def test_cost_overview_profileid_valid(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
@@ -159,35 +179,21 @@ class CostOverviewProfileidViewTest(CostOverviewViewTest):
 
         for profile_id in valid_profile_ids:
             for num_crews in valid_num_crews:
-                self._execute_profileid_test_case(profile_id, num_crews, status.HTTP_200_OK, test_case_source)
+                self.execute_test_case(status.HTTP_200_OK, test_case_source, profile_id=profile_id, num_crews=num_crews)
 
     def test_cost_overview_profileid_invalid_profile_id(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
         invalid_profile_ids = self.get_invalid_profile_ids()
         valid_num_crews = self.get_valid_num_crews()
 
-        for profile_id in invalid_profile_ids:
+        for invalid_profile_id in invalid_profile_ids:
             for num_crews in valid_num_crews:
-                self._execute_profileid_test_case(profile_id, num_crews, status.HTTP_400_BAD_REQUEST, test_case_source)
+                self.execute_test_case(status.HTTP_400_BAD_REQUEST, test_case_source, profile_id=invalid_profile_id, num_crews=num_crews)
     
     def test_cost_overview_profileid_invalid_num_crews(self):
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)  # type: ignore
-        valid_profile_id = self.get_valid_profile_ids()[0]
+        profile_id = self.get_valid_profile_ids()[0]
 
         for invalid_num_crews_group in invalid_input_groups['num_crews'].values():
-            for invalid_num_crews in invalid_num_crews_group:
-                if invalid_num_crews in ({}, []):  # Skip {} and [] in invalid checks - the serializer validates num_crews for them
-                    continue
-                self._execute_profileid_test_case(valid_profile_id, invalid_num_crews, status.HTTP_400_BAD_REQUEST, test_case_source)
-
-    def _execute_profileid_test_case(self, profile_id, num_crews, expected_status, test_case_source):
-        url = reverse('cost-overview-profile-v1', kwargs={'profile_id': profile_id})
-        response = self.client.get(url, {'num_crews': num_crews})
-        passed = response.status_code == expected_status
-        self.log_test_result(
-            passed=passed,
-            input_data={'profile_id': profile_id, 'num_crews': num_crews},
-            expected_message=expected_status,
-            actual_message=str(response.status_code),
-            test_case_source=test_case_source
-        )
+            invalid_num_crews = invalid_num_crews_group[0]
+            self.execute_test_case(status.HTTP_400_BAD_REQUEST, test_case_source, profile_id=profile_id, num_crews=invalid_num_crews)
