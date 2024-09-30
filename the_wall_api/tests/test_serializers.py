af17c3285fecefc924542442a7e4a6b7df892713
Aleksandar Dimitrov <adimitrov3@dxc.com>
2024-09-30 15:27:31 +0300

    Add type hinting in test_serializers -Remove unused methods: 	-extract_actual_errors 	-assert_error_matches -Reorganize methods in DailyIceUsageSerializerTest

--- the_wall_api/tests/test_serializers.py
+++ the_wall_api/tests/test_serializers.py
@@ -1,12 +1,14 @@
 from inspect import currentframe
+from typing import Any, Type
 
 from rest_framework import serializers
+from rest_framework.serializers import ListSerializer, Serializer, ValidationError
 
 from the_wall_api.serializers import CostOverviewSerializer, DailyIceUsageSerializer
 from the_wall_api.tests.test_utils import BaseTestcase, generate_valid_values, invalid_input_groups
 
 
-def extract_error_detail(actual_errors, field_name: str):
+def extract_error_detail(actual_errors: Any, field_name: str) -> Any:
     """Helper function to extract error details safely."""
     if isinstance(actual_errors, dict):
         error_detail = actual_errors.get(field_name, None)
@@ -19,7 +21,10 @@ def extract_error_detail(actual_errors, field_name: str):
 
 class SerializerTest(BaseTestcase):
 
-    def validate_and_log(self, serializer_class, input_data, expected_errors, test_case_source: str) -> None:
+    def validate_and_log(
+            self, serializer_class: Type[Serializer],
+            input_data: dict, expected_errors: dict, test_case_source: str
+    ) -> None:
         """Handles validation and logging of results."""
         actual_errors = None
         expect_errors = bool(expected_errors)
@@ -29,7 +34,7 @@ class SerializerTest(BaseTestcase):
             if expect_errors:
                 # We expect validation to fail and raise a ValidationError
                 validation_error = self.validate_with_errors(serializer, input_data)
-                actual_errors = validation_error.exception.detail
+                actual_errors = validation_error.detail
             else:
                 # We expect no errors, validation should pass
                 self.validate_without_errors(serializer)
@@ -44,35 +49,23 @@ class SerializerTest(BaseTestcase):
             actual_errors = f'{err.__class__.__name__}: {str(err)}'
             self.log_test_serializer_result(False, input_data, expected_errors, actual_errors, test_case_source, error_occured=True)
 
-    def validate_with_errors(self, serializer, input_data):
+    def validate_with_errors(self, serializer: Serializer | ListSerializer, input_data: dict) -> ValidationError:
         try:
-            with self.assertRaises(serializers.ValidationError) as validation_error:
+            with self.assertRaises(serializers.ValidationError) as validation_error_context:
                 serializer.is_valid(raise_exception=True)
         except AssertionError:
             self.fail(f'Expected ValidationError was not raised for input data: {input_data}')
         
-        return validation_error
+        return validation_error_context.exception
 
-    def validate_without_errors(self, serializer):
+    def validate_without_errors(self, serializer: Serializer | ListSerializer) -> None:
         is_valid = serializer.is_valid()
         self.assertTrue(is_valid)
 
-    def extract_actual_errors(self, serializer, expected_errors):
-        actual_errors = serializer.errors
-        for field, expected_error in expected_errors.items():
-            actual_error = extract_error_detail(actual_errors, field)
-            self.assert_error_matches(field, expected_error, actual_error)
-        return actual_errors
-
-    def assert_error_matches(self, field, expected_error, actual_error):
-        self.assertIn(field, actual_error)
-        if isinstance(expected_error, list):
-            for expected_msg in expected_error:
-                self.assertIn(expected_msg, actual_error if isinstance(actual_error, list) else [actual_error])
-        else:
-            self.assertIn(expected_error, actual_error if isinstance(actual_error, list) else [actual_error])
-
-    def log_test_serializer_result(self, passed, input_data, expected_errors, actual_errors, test_case_source, error_occured=False):
+    def log_test_serializer_result(
+            self, passed: bool, input_data: dict, expected_errors: dict,
+            actual_errors: Any, test_case_source: str, error_occured: bool = False
+    ):
         expected_message = ', '.join(expected_errors.values()) if expected_errors else 'No errors expected, validation passed'
         actual_message = 'Validation passed' if not actual_errors else ', '.join(
             [str(extract_error_detail(actual_errors, field)) for field in expected_errors.keys()]
@@ -133,6 +126,29 @@ class CostOverviewSerializerTest(SerializerTest):
 class DailyIceUsageSerializerTest(SerializerTest):
     description = 'Daily ice usage serializer tests'
 
+    def both_fields_invalid_inner(
+        self, invalid_profile_ids, invalid_days, profile_error_message, day_error_message, test_case_source
+    ):
+        """Helper function to test all combinations of profile and day values."""
+        for profile_id in invalid_profile_ids:
+            for day in invalid_days:
+                input_data = {'profile_id': profile_id, 'day': day}
+                expected_errors = {
+                    'profile_id': profile_error_message,
+                    'day': day_error_message,
+                }
+                with self.subTest(profile_id=profile_id, day=day):
+                    self.validate_and_log(DailyIceUsageSerializer, input_data, expected_errors, test_case_source)
+
+    def num_crews_invalid_inner(self, profile_id, day, test_case_source):
+        """Helper function to test all combinations of profile and day values."""
+        for error_message, invalid_num_crews in invalid_input_groups['num_crews'].items():
+            for num_crews in invalid_num_crews:
+                input_data = {'profile_id': profile_id, 'day': day, 'num_crews': num_crews}
+                expected_errors = {'num_crews': error_message}
+                with self.subTest(profile_id=profile_id, day=day, num_crews=num_crews):
+                    self.validate_and_log(DailyIceUsageSerializer, input_data, expected_errors, test_case_source)
+
     def test_both_fields_valid(self):
         valid_values = generate_valid_values()
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)    # type: ignore
@@ -181,20 +197,6 @@ class DailyIceUsageSerializerTest(SerializerTest):
                     test_case_source
                 )
 
-    def both_fields_invalid_inner(
-        self, invalid_profile_ids, invalid_days, profile_error_message, day_error_message, test_case_source
-    ):
-        """Helper function to test all combinations of profile and day values."""
-        for profile_id in invalid_profile_ids:
-            for day in invalid_days:
-                input_data = {'profile_id': profile_id, 'day': day}
-                expected_errors = {
-                    'profile_id': profile_error_message,
-                    'day': day_error_message,
-                }
-                with self.subTest(profile_id=profile_id, day=day):
-                    self.validate_and_log(DailyIceUsageSerializer, input_data, expected_errors, test_case_source)
-
     def test_num_crews_valid(self):
         valid_values = generate_valid_values()
         test_case_source = self._get_test_case_source(currentframe().f_code.co_name)    # type: ignore
@@ -213,12 +215,3 @@ class DailyIceUsageSerializerTest(SerializerTest):
         for profile_id in generate_valid_values():
             for day in generate_valid_values():
                 self.num_crews_invalid_inner(profile_id, day, test_case_source)
-
-    def num_crews_invalid_inner(self, profile_id, day, test_case_source):
-        """Helper function to test all combinations of profile and day values."""
-        for error_message, invalid_num_crews in invalid_input_groups['num_crews'].items():
-            for num_crews in invalid_num_crews:
-                input_data = {'profile_id': profile_id, 'day': day, 'num_crews': num_crews}
-                expected_errors = {'num_crews': error_message}
-                with self.subTest(profile_id=profile_id, day=day, num_crews=num_crews):
-                    self.validate_and_log(DailyIceUsageSerializer, input_data, expected_errors, test_case_source)
