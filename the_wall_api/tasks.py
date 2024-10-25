743b3190fe52bc56736093a8dae2a541e669e562
Aleksandar Dimitrov <adimitrov3@dxc.com>
2024-10-25 10:07:32 +0300

    Refactor tasks.py -Externalize error logging in separate helper functions

--- the_wall_api/tasks.py
+++ the_wall_api/tasks.py
@@ -3,34 +3,37 @@ from celery import shared_task
 from the_wall_api.utils.celery_task_utils import archive_logs, clean_old_archives, log_error
 
 
-# === Scheduled tasks ===
-@shared_task(queue='file_tasks')    # Task sent to 'file_tasks' queue
+# Helper Functions
+def execute_task_with_error_handling(task_func, *args, **kwargs) -> None:
+    try:
+        return task_func(*args, **kwargs)
+    except Exception as unknwn_err:
+        send_log_error(unknwn_err)
+
+
+def send_log_error(unknwn_err: Exception) -> None:
+    """Logs the error details asynchronously."""
+    from the_wall_api.utils.error_utils import extract_error_traceback
+
+    error_message = f'{unknwn_err.__class__.__name__}: {str(unknwn_err)}'
+    error_traceback = extract_error_traceback(unknwn_err)
+    log_error_task.delay('celery_tasks', error_message, error_traceback)    # type: ignore
+
+
+# === Sequential tasks ===
+
+# File Tasks
+@shared_task(queue='file_tasks')
 def archive_logs_task(*args, **kwargs) -> None:
-    from the_wall_api.utils.error_utils import extract_error_traceback
-
-    try:
-        archive_logs(*args, **kwargs)
-    except Exception as unknwn_err:
-        error_message = f'{unknwn_err.__class__.__name__}: {str(unknwn_err)}'
-        error_traceback = extract_error_traceback(unknwn_err)
-        log_error_task.delay('celery_tasks', error_message, error_traceback)    # type: ignore
+    execute_task_with_error_handling(archive_logs, *args, **kwargs)
 
 
-@shared_task(queue='file_tasks')    # Task sent to 'file_tasks' queue
+@shared_task(queue='file_tasks')
 def clean_old_archives_task(*args, **kwargs) -> None:
-    from the_wall_api.utils.error_utils import extract_error_traceback
-
-    try:
-        clean_old_archives(*args, **kwargs)
-    except Exception as unknwn_err:
-        error_message = f'{unknwn_err.__class__.__name__}: {str(unknwn_err)}'
-        error_traceback = extract_error_traceback(unknwn_err)
-        log_error_task.delay('celery_tasks', error_message, error_traceback)    # type: ignore
-
-# === Scheduled tasks end ===
+    execute_task_with_error_handling(clean_old_archives, *args, **kwargs)
 
 
-@shared_task(queue='file_tasks')    # Task sent to 'file_tasks' queue
+@shared_task(queue='file_tasks')
 def log_error_task(*args, **kwargs) -> str:
     from the_wall_api.utils.error_utils import extract_error_traceback
 
@@ -44,3 +47,6 @@ def log_error_task(*args, **kwargs) -> str:
             print(line)
 
     return error_id
+
+
+# === Sequential tasks end ===
