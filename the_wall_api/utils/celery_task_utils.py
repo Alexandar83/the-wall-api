26d3418b51328c948c8272e8397814e3df0895d8
Aleksandar Dimitrov <adimitrov3@dxc.com>
2024-10-25 18:29:57 +0300

    Refactor celery_task_utils -Reorder methods

--- the_wall_api/utils/celery_task_utils.py
+++ the_wall_api/utils/celery_task_utils.py
@@ -8,9 +8,9 @@ from shutil import copyfileobj
 from time import sleep
 
 LIGHT_CELERY_CONFIG = os.getenv('LIGHT_CELERY_CONFIG', False) == 'True'
+DELETION_RETRIES = 5
 
 if not LIGHT_CELERY_CONFIG:
-
     from django.conf import settings
 
     BUILD_SIM_LOGS_DIR = settings.BUILD_SIM_LOGS_DIR
@@ -19,8 +19,6 @@ if not LIGHT_CELERY_CONFIG:
     BUILD_SIM_LOGS_ARCHIVE_RETENTION_DAYS = settings.BUILD_SIM_LOGS_ARCHIVE_RETENTION_DAYS
 
     CELERY_BROKER_URL = settings.CELERY_BROKER_URL
-
-DELETION_RETRIES = 5
 
 
 def archive_logs(input_params: dict | None = None, test_input_params: dict | None = None) -> None:
@@ -43,15 +41,43 @@ def archive_logs(input_params: dict | None = None, test_input_params: dict | Non
 
             # Archive the file if it's older than the retention period
             if file_mtime < log_retention_date:
-                archive_path = os.path.join(logs_arhive_dir, log_file + '.gzip')
+                archive_file(logs_arhive_dir, log_file, log_path)
 
-                # Compress the log file and copy it to the archive
-                with open(log_path, 'rb') as f_in:
-                    with gzip_open(archive_path, 'wb', compresslevel=9) as f_out:
-                        copyfileobj(f_in, f_out)
 
-                # Delete the original log file after compressing
-                remove_file(log_path)
+def get_archive_logs_details(input_params: dict | None = None, test_input_params: dict | None = None) -> tuple[datetime, str, str]:
+    now = datetime.now()
+
+    if input_params and input_params['logs_type'] == 'build_sim':
+        log_retention_date = now - timedelta(days=BUILD_SIM_LOGS_RETENTION_DAYS)
+        return log_retention_date, BUILD_SIM_LOGS_DIR, BUILD_SIM_LOGS_ARCHIVE_DIR
+
+    if test_input_params and test_input_params['logs_type'] == 'build_sim':
+        log_retention_date = now
+        logs_dir, logs_archive_dir, _ = get_test_log_archive_details(
+            BUILD_SIM_LOGS_DIR, test_input_params['test_file_name']
+        )
+        return log_retention_date, logs_dir, logs_archive_dir
+
+    raise ValueError('Invalid or missing logs type')
+
+
+def get_test_log_archive_details(root_dir: str, test_file_name: str) -> tuple[str, str, str]:
+    test_logs_dir = os.path.join(root_dir, 'testing', 'test_logs')
+    test_logs_dir_archive = os.path.join(test_logs_dir, 'archive')
+    test_file = os.path.join(test_logs_dir, test_file_name)
+
+    return test_logs_dir, test_logs_dir_archive, test_file
+
+
+def archive_file(logs_arhive_dir: str, log_file: str, log_path: str,) -> None:
+    archive_path = os.path.join(logs_arhive_dir, log_file + '.gzip')
+
+    # Compress the log file and copy it to the archive
+    with open(log_path, 'rb') as f_in, gzip_open(archive_path, 'wb', compresslevel=9) as f_out:
+        copyfileobj(f_in, f_out)
+
+    # Delete the original log file after compressing
+    remove_file(log_path)
 
 
 def clean_old_archives(input_params: dict | None = None, test_input_params: dict | None = None) -> None:
@@ -71,64 +97,21 @@ def clean_old_archives(input_params: dict | None = None, test_input_params: dict
                 remove_file(archive_path)
 
 
-def log_error(error_type: str, error_message: str, error_traceback: str, request_info: dict = {}, error_id_prefix: str = '') -> str:
-    from redis import Redis
-
-    # Redis DB2 is used for persistent data
-    redis_connection = Redis.from_url(CELERY_BROKER_URL)
-    error_id = error_id_prefix + str(redis_connection.incr('unknown_errors_counter'))
-
-    logger = logging.getLogger(error_type)
-    logger.error(error_message, extra={'traceback': error_traceback, 'request_info': request_info, 'error_id': error_id})
-
-    return error_id
-
-
-def get_archive_logs_details(input_params: dict | None = None, test_input_params: dict | None = None) -> tuple[datetime, str, str]:
-    now = datetime.now()
-
-    if input_params:
-        if input_params['logs_type'] == 'build_sim':
-            log_retention_date = now - timedelta(days=BUILD_SIM_LOGS_RETENTION_DAYS)
-            return log_retention_date, BUILD_SIM_LOGS_DIR, BUILD_SIM_LOGS_ARCHIVE_DIR
-
-    if test_input_params:
-        if test_input_params['logs_type'] == 'build_sim':
-            log_retention_date = now
-            logs_dir, logs_archive_dir, _ = get_test_log_archive_details(
-                BUILD_SIM_LOGS_DIR, test_input_params['test_file_name']
-            )
-            return log_retention_date, logs_dir, logs_archive_dir
-
-    raise ValueError('Invalid or missing logs type')
-
-
 def get_clean_old_archives_details(input_params: dict | None = None, test_input_params: dict | None = None) -> tuple[datetime, str]:
     now = datetime.now()
 
-    if input_params:
-        if input_params['logs_type'] == 'build_sim':
-            archive_retention_date = now - timedelta(days=BUILD_SIM_LOGS_ARCHIVE_RETENTION_DAYS)
-            return archive_retention_date, BUILD_SIM_LOGS_ARCHIVE_DIR
+    if input_params and input_params['logs_type'] == 'build_sim':
+        archive_retention_date = now - timedelta(days=BUILD_SIM_LOGS_ARCHIVE_RETENTION_DAYS)
+        return archive_retention_date, BUILD_SIM_LOGS_ARCHIVE_DIR
 
-    if test_input_params:
-        if test_input_params['logs_type'] == 'build_sim':
-            archive_retention_date = now
-            _, logs_archive_dir, _ = get_test_log_archive_details(
-                BUILD_SIM_LOGS_DIR, test_input_params['test_file_name']
-            )
-            return archive_retention_date, logs_archive_dir
+    if test_input_params and test_input_params['logs_type'] == 'build_sim':
+        archive_retention_date = now
+        _, logs_archive_dir, _ = get_test_log_archive_details(
+            BUILD_SIM_LOGS_DIR, test_input_params['test_file_name']
+        )
+        return archive_retention_date, logs_archive_dir
 
     raise ValueError('Invalid or missing logs type')
-
-
-def get_test_log_archive_details(root_dir: str, test_file_name: str) -> tuple[str, str, str]:
-    testing_dir_name = 'testing'
-    test_logs_dir = os.path.join(root_dir, testing_dir_name, 'test_logs')
-    test_logs_dir_archive = os.path.join(test_logs_dir, 'archive')
-    test_file = os.path.join(test_logs_dir, test_file_name)
-
-    return test_logs_dir, test_logs_dir_archive, test_file
 
 
 def remove_file(file_path: str) -> None:
@@ -144,3 +127,17 @@ def remove_file(file_path: str) -> None:
                 deletion_retry_delay += 2
                 print(f'Failed to delete {file_path} - access denied. Retrying...')
                 sleep(deletion_retry_delay)
+
+
+def log_error(error_type: str, error_message: str, error_traceback: str, request_info: dict = {}, error_id_prefix: str = '') -> str:
+    """Log unexpected app errors sequentially, avoiding race conditions during log files updates."""
+    from redis import Redis
+
+    # Redis DB2 is used for persistent data
+    redis_connection = Redis.from_url(CELERY_BROKER_URL)
+    error_id = error_id_prefix + str(redis_connection.incr('unknown_errors_counter'))
+
+    logger = logging.getLogger(error_type)
+    logger.error(error_message, extra={'traceback': error_traceback, 'request_info': request_info, 'error_id': error_id})
+
+    return error_id
