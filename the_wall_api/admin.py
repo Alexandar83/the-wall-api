a9bc5e54edf9a66575641c70610dff0a71e50e13
Aleksandar Dimitrov <sasho_1983@yahoo.com>
2024-11-30 18:48:52 +0200

    Custom WallProfileProgress admin console filter

--- the_wall_api/admin.py
+++ the_wall_api/admin.py
@@ -4,29 +4,46 @@ from the_wall_api.models import (
 )
 
 
+class WallConfigFilter(admin.SimpleListFilter):
+    """Custom filter for WallConfig."""
+    title = 'Wall Config'
+    parameter_name = 'wall_config'
+
+    def lookups(self, request, model_admin):
+        # Fetch WallConfig objects to display as filter options
+        wall_configs = WallConfig.objects.all()
+        return [(config.pk, str(config)) for config in wall_configs]
+
+    def queryset(self, request, queryset):
+        # Filter queryset based on selected WallConfig
+        if self.value():
+            return queryset.filter(wall_profile__wall__wall_config_id=self.value())
+        return queryset
+
+
 class WallConfigReferenceAdmin(admin.ModelAdmin):
-    list_filter = ('user', 'wall_config', 'config_id')
-    search_fields = ('user', 'wall_config', 'config_id', 'date_created')
+    list_filter = ['user', 'wall_config', 'config_id']
+    search_fields = ['user', 'wall_config', 'config_id', 'date_created']
 
 
 class WallConfigAdmin(admin.ModelAdmin):
-    list_filter = ('id', 'status')
-    search_fields = ('id', 'wall_config_hash', 'status', 'date_created')
+    list_filter = ['id', 'status']
+    search_fields = ['id', 'wall_config_hash', 'status', 'date_created']
 
 
 class WallAdmin(admin.ModelAdmin):
-    list_filter = ('id', 'wall_config_hash')
-    search_fields = ('id', 'wall_config_hash', 'num_crews', 'total_cost', 'construction_days', 'date_created')
+    list_filter = ['wall_config']
+    search_fields = ['id', 'wall_config_hash', 'num_crews', 'total_cost', 'construction_days', 'date_created']
 
 
 class WallProfileAdmin(admin.ModelAdmin):
-    list_filter = ('id', 'wall', 'wall_profile_config_hash')
-    search_fields = ('id', 'wall_profile_config_hash', 'profile_id', 'cost', 'date_created')
+    list_filter = ['wall']
+    search_fields = ['id', 'wall_profile_config_hash', 'profile_id', 'cost', 'date_created']
 
 
 class WallProfileProgressAdmin(admin.ModelAdmin):
-    list_filter = ('wall_profile', 'day')
-    search_fields = ('id', 'day', 'ice_used', 'date_created')
+    list_filter = [WallConfigFilter]
+    search_fields = ['id', 'day', 'ice_used', 'date_created']
 
 
 admin.site.register(WallConfigReference, WallConfigReferenceAdmin)
